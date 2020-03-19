27c96dfd3300341f5dd190a87e0036592ee68f0f
rjmcgill <rjmcgill@ladeheadu.ca>
2020-03-18 22:14:37 -0400

    Created the Barebones website

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset = "utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      //Connect to the database
+      $db = new PDO('mysql:host=172.31.22.43;dbname=Ryan_J1103749', 'Ryan_J1103749', 'DqwMH5MD1Z');
+
+    ?>
+  </body>
+</html>
