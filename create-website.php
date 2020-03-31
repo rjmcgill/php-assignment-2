cf61efa5774b4d64e9927df8235a5b3064629f3f
rjmcgill <rjmcgill@ladeheadu.ca>
2020-03-30 23:08:00 -0400

    Saving a webpage now works

--- create-website.php
+++ create-website.php
@@ -5,9 +5,27 @@
     header("location:login.php");
   }
 ?>
-    <div>
-      <h1>Create a Website</h1>
-      <p>Wow what a great placeholder</p>
-    </div>
+
+    <main class="container">
+        <h1>Create a Website</h1>
+        <form method="post" action= "save-website.php">
+            <fieldset class="form-group">
+                <label for="name" class="col-md-2">Website Name:</label>
+                <input name="name" id="name" placeholder="Name" required/>
+            </fieldset>
+            <fieldset class="form-group">
+                <label for="content" class="col-md-2">Page Content(Max 1028 characters): </label>
+                <style>
+                  textarea {
+                    width: 100%;
+                  }
+                </style>
+                <textarea name="content" id="content" required></textarea>
+            </fieldset>
+            <div>
+                <input type="submit" value="Create" class="btn btn-info"/>
+            </div>
+        </form>
+    </main>
   </body>
 </html>
--- save-website.php
+++ save-website.php
@@ -4,9 +4,60 @@
   if(empty($_SESSION['username'])) {
     header("location:login.php");
   }
+
+  //Store form inputs in variables
+  $website_Title = $_POST['name'];
+  $website_Content = $_POST['content'];
+
+
+
+  //validate inputs
+  $ok = true;
+  if(empty($website_Title)) {
+    echo 'Name is required <br>';
+    $ok = false;
+  }
+  if(empty($website_Content)) {
+    echo 'Content is required <br>';
+    $ok = false;
+  }
+
+  if($ok) {
+    try {
+      //connect
+      $db = new PDO('mysql:host=172.31.22.43;dbname=Ryan_J1103749', 'Ryan_J1103749', 'DqwMH5MD1Z');
+
+      //duplicate check before insert
+      $sql = "SELECT * FROM websites WHERE website_Title = :website_Title";
+      $cmd = $db->prepare($sql);
+      $cmd->bindParam(':website_Title', $website_Title, PDO::PARAM_STR, 45);
+
+      $cmd->execute();
+      $websites = $cmd->fetch();
+
+      if(!empty($websites)) {
+        echo 'Website Name already taken';
+        header("location:create-website.php?taken=true");
+      } else {
+        //set up & run insert
+        $sql = "INSERT INTO websites (website_Title, website_Content) VALUES (:website_Title, :website_Content)";
+
+        $cmd = $db->prepare($sql);
+        $cmd->bindParam(':website_Title', $website_Title, PDO::PARAM_STR, 45);
+        $cmd->bindParam(':website_Content', $website_Content, PDO::PARAM_STR, 1028);
+        $cmd->execute();
+
+        //Disconnect
+        $db = null;
+        //redirect to login page
+        //header("location:index.php");
+      }
+    } catch(Execption $e) {
+        echo 'Oops! Something broke!';
+      }
+  }
+
+
 ?>
-    <div>
-      <p>Wow what a great placeholder</p>
-    </div>
   </body>
 </html>
