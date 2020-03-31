848cd1371c5931085b0980cc02be84a68b52c780
rjmcgill <rjmcgill@ladeheadu.ca>
2020-03-31 15:34:01 -0400

    Implimented Error Handling

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+  $title = 'Page Not Found';
+  require_once('navbar.php');
+ ?>
+
+     <h1> Ooooooooooops </h1>
+
+     <p>Sorry but we couldn't find the page you were looking for</p>
+     <p>Please use the links in the header</p>
+
+  </body>
+</html>
